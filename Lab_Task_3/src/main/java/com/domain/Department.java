1d26d2da8455d848d5ce5776a80e3478bc7bc282
rizwanulhassan-eng <65031599+rizwanulhassan-eng@users.noreply.github.com>
2022-11-16 22:51:17 +0600

    Add files via upload

---
+++ Lab_Task_3/src/main/java/com/domain/Department.java
@@ -0,0 +1,38 @@
+package com.domain;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+public class Department {
+
+    private int id;
+
+    @NotNull
+    private String name;
+
+    private LocalDate createdOn;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(LocalDate createdOn) {
+        this.createdOn = createdOn;
+    }
+}
